3f7e1d408af1aa02aec32ff240bde46bde508279
ricardovieira <ricardo.vieira@xpand-it.com>
2018-03-08 12:38:26 +0000

    1 - jitpack fixing

--- sample/src/main/java/com/rjsvieira/recyclerviewmultipleviewtypesadapter/sample/MainActivity.java
+++ sample/src/main/java/com/rjsvieira/recyclerviewmultipleviewtypesadapter/sample/MainActivity.java
@@ -32,8 +32,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 1; i <= 4; i++) {
             SampleData data = new SampleData();
             data.mTitle = getString(R.string.title_type2);
-            data.mDrawableResId = getResources().getIdentifier(
-                    getString(R.string.drawable_animal_name, i), "drawable", getPackageName());
+            data.mDrawableResId = getResources().getIdentifier(getString(R.string.drawable_animal_name, i), "drawable", getPackageName());
             data.mContent = getString(R.string.content_type2, i);
             dataSet.add(data);
         }
