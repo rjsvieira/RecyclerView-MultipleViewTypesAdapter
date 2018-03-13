8d958d75cf5b67fd287e73043804e8a003af1893
ricardovieira <ricardo.vieira@xpand-it.com>
2018-03-13 15:50:12 +0000

    1 - adding the BaseViewHolder to the project

---
+++ library/src/main/java/com/rjsvieira/recyclerviewmultipleviewtypesadapter/views/BaseViewHolder.java
@@ -0,0 +1,13 @@
+package com.rjsvieira.recyclerviewmultipleviewtypesadapter.views;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+public class BaseViewHolder extends RecyclerView.ViewHolder {
+
+    public BaseViewHolder(View itemView) {
+        super(itemView);
+    }
+
+}
